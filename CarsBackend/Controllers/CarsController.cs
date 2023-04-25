8596836210ec33e19004a69532e8ad603fd9ac74
Felipe Serna <feserna86@gmail.com>
2023-04-25 17:23:09 -0500

    GET and POST (buy) in back

---
+++ CarsBackend/Controllers/CarsController.cs
@@ -0,0 +1,36 @@
+﻿using CarsBackend.Data;
+using CarsBackend.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace CarsBackend.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CarsController : Controller
+    {
+        private readonly CarsBackendDBContext _carsBackendDBContext;
+        public CarsController(CarsBackendDBContext carsBackendDBContext)
+        {
+            _carsBackendDBContext = carsBackendDBContext;
+
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetAllCars()
+        {
+            var cars = await _carsBackendDBContext.Cars.ToListAsync();
+            return Ok(cars);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> BuyCar([FromBody] Car carRequest)
+        {
+            carRequest.Id = Guid.NewGuid();
+
+            await _carsBackendDBContext.Cars.AddAsync(carRequest);
+            await _carsBackendDBContext.SaveChangesAsync();
+
+            return Ok(carRequest);
+        }
+    }
+}
