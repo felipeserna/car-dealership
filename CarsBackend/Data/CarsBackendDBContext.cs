ebac38cab3dad0161b470e185d6e983ef06417c9
Felipe Serna <feserna86@gmail.com>
2023-04-25 16:31:24 -0500

    Before db migration

---
+++ CarsBackend/Data/CarsBackendDBContext.cs
@@ -0,0 +1,14 @@
+﻿using CarsBackend.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CarsBackend.Data
+{
+    public class CarsBackendDBContext : DbContext
+    {
+        public CarsBackendDBContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Car> Cars { get; set; }
+    }
+}
---
+++ CarsBackend/Models/Car.cs
@@ -0,0 +1,17 @@
+﻿namespace CarsBackend.Models
+{
+    public class Car
+    {
+        public Guid Id { get; set; }
+        
+        public string Brand { get; set; }
+
+        public string Model { get; set; }
+
+        public int Year { get; set; }
+
+        public bool IsNew { get; set; }
+
+        public int Price { get; set; }
+    }
+}
